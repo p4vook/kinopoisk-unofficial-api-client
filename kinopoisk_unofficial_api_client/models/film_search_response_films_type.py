3f1238af906bb274698b1a0ab30662d6845ff0ea
Pavel Kalugin <pavel@pavelthebest.me>
2024-05-20 20:37:25 +0300

    fix OpenAPI schema bug: TV_SERIES

--- kinopoisk_unofficial_api_client/models/film_search_response_films_type.py
+++ kinopoisk_unofficial_api_client/models/film_search_response_films_type.py
@@ -4,6 +4,7 @@ from enum import Enum
 class FilmSearchResponseFilmsType(str, Enum):
     FILM = "FILM"
     TV_SHOW = "TV_SHOW"
+    TV_SERIES = "TV_SERIES"
     UNKNOWN = "UNKNOWN"
 
     def __str__(self) -> str:
